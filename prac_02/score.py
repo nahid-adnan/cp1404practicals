0ace52f79d403c852b9cb392607cf728b9324288
B M Nahid Hasan Adnan <bmnahidhasan.adnan@my.jcu.edu.au>
2026-07-02 20:08:23 +1000

    Refactor score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,11 +3,18 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
+import random
+
 
 def main():
     score = float(input("Enter your score :"))
     result = get_result(score)
     print(f" User {score} is {result}")
+    if result == "Excellent":
+        print("You get a prize!")
+    random_score = random.randint(0, 100)
+    result = get_result(random_score)
+    print(f"Random:{random_score} = {result}")
 
 
 def get_result(score):
