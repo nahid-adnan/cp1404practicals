d7c84d6f1dff281e91c6e7de476e747e7fe15078
B M Nahid Hasan Adnan <bmnahidhasan.adnan@my.jcu.edu.au>
2026-07-04 17:43:13 +1000

    Refactor score menu to use main() function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -8,27 +8,33 @@ P- print result
 S- show stars
 Q- quit """
 
-score = 0
-print(Menu)
-choice = input(">>>").upper()
-while choice != "Q":
-    if choice == "G":
-        score = float(input("Enter your score: "))
-        print(score)
-    elif choice == "P":
-        if score < 0 or score > 100:
-            print("Invalid score")
-        elif score >= 90:
-            print("Excellent")
-        elif score >= 50:
-            print("passable")
-        else:
-            print("Bad")
-    elif choice == "S":
-        stars = "*" * int(score // 10)
-        print(stars)
-    else:
-        print("Invalid option")
+
+def main():
     print(Menu)
-    choice = input(">>> ").upper()
-print("Thank you")
+    choice = input(">>>").upper()
+    score = 0
+    while choice != "Q":
+        if choice == "G":
+            score = float(input("Enter your score: "))
+            print(score)
+        elif choice == "P":
+            if score < 0 or score > 100:
+                print("Invalid score")
+            elif score >= 90:
+                print("Excellent")
+            elif score >= 50:
+                print("passable")
+            else:
+                print("Bad")
+        elif choice == "S":
+            stars = "*" * int(score // 10)
+            print(stars)
+        else:
+            print("Invalid option")
+        print(Menu)
+        choice = input(">>> ").upper()
+
+    print("Thank you")
+
+
+main()
