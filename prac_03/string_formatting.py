7b2d408b03ff1f0a1d6abc3bc99d740d6f8e70fb
B M Nahid Hasan Adnan <bmnahidhasan.adnan@my.jcu.edu.au>
2026-07-05 15:03:33 +1000

    Add string formatting guitar example

---
+++ prac_03/string_formatting.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical-String formatting examples
+Name: B M Nahid Hasan Adnan
+Date: 05/07/2026
+"""
+
+year = 1922
+guitar_model = "Gibson L-5 CES"
+price = 16036.18
+
+print(f"{year} {guitar_model} for about ${price:,.0f}!")
