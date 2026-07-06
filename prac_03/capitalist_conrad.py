75225f159eeffa28aba13a5f952f2f0769a440c4
B M Nahid Hasan Adnan <bmnahidhasan.adnan@my.jcu.edu.au>
2026-07-06 13:35:52 +1000

    Write Capitalist Conrad output to file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -15,8 +15,11 @@ MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 
+FILENAME = "prices.txt"
+out_file = open(FILENAME, 'w')
+
 price = INITIAL_PRICE
-print(f"Starting price: ${price:,.2f}")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 number_of_days = 0
 
 while MIN_PRICE <= price <= MAX_PRICE:
@@ -34,4 +37,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
     number_of_days += 1
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
